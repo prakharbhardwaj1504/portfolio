d2b1b3dd46af80b23e2850d7882989ebd9a1f4d0
prakharbhardwaj1504 <prakharbhardwaj1504@gmail.com>
2024-01-19 03:51:26 +0530

    intro v1.0
    I have centered main lines, set there style and font and spacing and sizing...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "./componets/Navbar/navbar";
+import Intro1 from "./componets/intro1/intro1"
 function App() {
   return (
     <div className="App">
     <Navbar />
-     
+    <Intro1 />
     </div>
   );
 }
---
+++ src/componets/intro1/intro1.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./intro1.css";
+// ///////////////////////////////////
+
+
+// ///////////////////////////////////
+
+
+
+function intro1() {
+
+  return (
+    <main className="main">
+      
+      <div className="hi">hi, i'm Prakhar Bhardwaj </div>
+      <div className="full_stack">A </div>
+
+
+      <div class="container">
+      <div class="text"><div className="full_stack">Full Stack</div></div>
+    </div>
+      
+
+
+
+      <div className="full_stack">Developer</div>
+
+    </main>
+  );
+}
+
+export default intro1;
