602e6175d392333fe108f5307d93fad4ed5781f9
prakharbhardwaj1504 <prakharbhardwaj1504@gmail.com>
2024-02-15 19:55:50 +0530

    added babel private property plugin

--- src/componets/about/about.js
+++ src/componets/about/about.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import "./about.css";
 import { annotate } from "https://unpkg.com/rough-notation?module";
 import gsap from "gsap";
