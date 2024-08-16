9ffa583bab3af6f17fd3905b6e1c807e4e69c91b
prakharbhardwaj1504 <prakharbhardwaj1504@gmail.com>
2024-08-16 22:29:08 +0530

    trying to change resume link with drive link

--- src/componets/resume/resume.js
+++ src/componets/resume/resume.js
@@ -3,7 +3,8 @@ import "./resume.css";
 
 function Resume() {
 
-    const imagePath = process.env.PUBLIC_URL + "/favicon/RA2111003030420_PRAKHAR_BHARDWAJ CV";
+    // const imagePath = process.env.PUBLIC_URL + "/favicon/RA2111003030420_PRAKHAR_BHARDWAJ CV";
+    const imagePath="https://drive.google.com/file/d/1ln_LVkDSKyfdNW40c9QnwRm8WKRT8Q99/view?usp=drive_link"
   
   return ( <div className="resume">Resume
   
